cc95ad8e96ba566789d46a92709e677b584d31b7
league <league@imac-12.attlocal.net>
2022-05-05 16:39:56 -0700

    start boolean

--- src/_01_double/_2_test_scores/test_Scores.java
+++ src/_01_double/_2_test_scores/test_Scores.java
@@ -10,12 +10,12 @@ public static void main(String[] args) {
 		String testScore = JOptionPane.showInputDialog(null, "what did u get on your test?");
 		double score = Double.parseDouble(testScore);
 		if (score<85) {
-			System.out.println("you need to study more");
+			System.out.println("Do better next time, and remember to study harder!!");
 		}
-		else {
-			System.out.println("good job!!!!");
+		else{
+			System.out.println("Good Job! Your hard work has finally paid off!");
 		}
-		System.out.println(score);
+	
 
 	
 }
